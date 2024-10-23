899534315cb1700d5d8c9442247a2043d908a4d2
Solomon Banuba <101892794+Solomon-Banuba@users.noreply.github.com>
2024-10-23 23:55:52 +0200

    Update Window_functions.sql

--- Window_functions.sql
+++ Window_functions.sql
@@ -1,3 +1,10 @@
+-- WINDOW FUNCTIONS AKA ANALYTICAL FUNCTIONS
+/* Performs functiomns aggrgegrat operations on groups of rows but they produce a result  FOR EACH ROW. */
+
+
+
+-- OVER CLAUSE: Constucts a window. When it's empty, the window will include all records
+
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY AUTO_INCREMENT,
     department VARCHAR(20),
@@ -28,7 +35,16 @@ INSERT INTO employees (department, salary) VALUES
 ('customer service', 55000);
  
  
-SELECT emp_no, department, salary, AVG(salary) OVER() FROM employees;
+ -- Average salaries for the entire table 
+SELECT 
+  emp_no, 
+  department, 
+  salary, 
+  AVG(salary) OVER()
+ FROM employees;
+ 
+ 
+ -- MIN and MAX salaries for the entire table
  
 SELECT 
     emp_no, 
@@ -40,6 +56,77 @@ FROM employees;
     
     
 SELECT 
-    emp_no, department, salary, MIN(salary), MAX(salary)
-FROM
-    employees;
+    emp_no,
+    department, 
+    salary, 
+    MIN(salary), 
+    MAX(salary)
+FROM employees
+GROUP BY   emp_no,
+    department,  salary;
+    
+    
+    
+-- PARTITION BY CLAUSE WITH OVER: OVER(PARTITION BYS column_name)
+-- Inside of the the OVER(), us the PARTITION BY to form rows into group of row
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    AVG(salary) OVER(PARTITION BY department) AS dept_avg,
+    AVG(salary) OVER() AS company_avg
+FROM employees;
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    COUNT(*) OVER(PARTITION BY department) as dept_count
+FROM employees;
+ 
+
+
+WITH aggr_cal AS(
+  SELECT 
+    emp_no
+    , department
+    , salary
+    , ROUND(AVG(salary) OVER(PARTITION BY department), 2)AS dept_avg
+    , ROUND(AVG(salary) OVER(),2 ) AS company_avg
+  FROM employees
+ 
+)
+
+  SELECT 
+    emp_no 
+    , department
+    , salary
+    , dept_avg
+    , company_avg
+    , CASE 
+        WHEN dept_avg > company_avg THEN 'high_earner'
+        ELSE 'below_avg'
+      END AS salary_status
+ FROM aggr_cal;
+
+
+
+
+-- OVER + PARTITION +ORDER BY 
+-- ORDER BY inside of the OVER() cluase to re-order rows within each window
+SELECT 
+    emp_no
+    , department
+    , salary
+    , SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_dept_salary
+    , SUM(salary) OVER(PARTITION BY department) AS total_dept_salary
+FROM employees;
+
+--
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    MIN(salary) OVER(PARTITION BY department ORDER BY salary DESC) as rolling_min
+FROM employees;
